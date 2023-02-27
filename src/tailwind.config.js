1cbd42f29547596891ed26fdd74b66417f3057e2
drakon007 <73273560+drakon007@users.noreply.github.com>
2023-02-27 15:20:35 +0800

    переход на tailwind
    надо доделать стили

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'panel-grey': '#2D3C60',
+        'white': '#ffffff'
+      },
+      spacing: {
+        '300': '300px',
+        '250': '250px',
+        '100': '100px',
+        '50': '50px'
+      }
+    },
   },
   plugins: [],
 }
