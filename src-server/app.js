e90acfa75f0a56e488cc5dd16043df2a53564eae
drakon007 <73273560+drakon007@users.noreply.github.com>
2023-04-11 13:41:28 +0800

    add scr-server
    настройка сервера

---
+++ src-server/app.js
@@ -0,0 +1,32 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+const app= express();
+import mongoose from 'mongoose';
+
+
+app.use(express.json());
+
+// внешние переменные созданного хоста
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+
+// внешние переменные подключения к mongoBD
+const BD_HOST = process.env.BD_HOST;
+const BD_PORT = process.env.BD_PORT;
+const BD_NAME = process.env.BD_NAME;
+
+// функция для подключения к базе данных
+async function Connect() {
+
+    try {
+        await mongoose.connect(`mongodb://${BD_HOST}:${BD_PORT}/${BD_NAME}`);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.log('Connection error', error.message);
+    }
+    console.log(`Server running at http://${HOST}:${PORT}`);
+
+}
+
+app.listen(PORT, Connect);
